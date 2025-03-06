853aa25fa2d9d034750d2a8153da88303a8d9688
unknown <jinwu2003@gmail.com>
2025-03-05 21:07:06 -0800

    lab 21b

--- routing-lab/src/images/ImageDetails.jsx
+++ routing-lab/src/images/ImageDetails.jsx
@@ -3,20 +3,24 @@ import { MainLayout } from "../MainLayout.jsx";
 import { useImageFetching } from "./useImageFetching.js";
 
 export function ImageDetails(props) {
-    const { imageId } = useParams();
-    const { isLoading, fetchedImages } = useImageFetching(imageId, 500);
-    if (isLoading) {
-        return <p>Loading...</p>;
-    }
-    const imageData = fetchedImages[0];
-    if (!imageData) {
-        return <h2>Image not found</h2>;
-    }
+  const { imageId } = useParams();
+  const { isLoading, fetchedImages } = useImageFetching(imageId);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  const imageData = fetchedImages[0];
+  if (!imageData) {
+    return <h2>Image not found</h2>;
+  }
 
-    return (
-        <>
-            <h2>{imageData.name}</h2>
-            <img className="ImageDetails-img" src={imageData.src} alt={imageData.name} />
-        </>
-   )
+  return (
+    <>
+      <h2>{imageData.name}</h2>
+      <img
+        className="ImageDetails-img"
+        src={imageData.src}
+        alt={imageData.name}
+      />
+    </>
+  );
 }
